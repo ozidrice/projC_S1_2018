de3e5f363702557bee1162b6ce3b603d95b81432
Noowl <noeliebravo@laposte.net>
2018-01-13 14:03:07 +0000

    suppression com

--- jeu.c
+++ jeu.c
@@ -1,19 +1,19 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
 #include "unite.h"
 #include "monde.h"
-=======
+
 #include "monde.h"
 #include "unite.h"
->>>>>>> 88fad3fa8ae578fe7ce4af1a16ee382d8ecb956e
-=======
+
+
 #include "monde.h"
 #include "unite.h"
-<<<<<<< HEAD
+
 #include "joueur.h"
-=======
->>>>>>> 88fad3fa8ae578fe7ce4af1a16ee382d8ecb956e
->>>>>>> 6d454319c10e12fba454cc537b45ff9feaae2cd7
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -30,45 +30,45 @@ void printDelimiteur(){
 
 void menu(){
 	int choice;
-<<<<<<< HEAD
+
 	printf("[0]: Quitter\n[1]: Afficher le monde\n");
 	printf("Votre choix: ");
 
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
+
 	printf("Choix : ");
 	scanf("%d",&choice);
 
 
-=======
+
 	printf("[0]: Quitter\n[1]: Afficher le monde\nVotre choix:");
->>>>>>> 6d454319c10e12fba454cc537b45ff9feaae2cd7
+
 	scanf("%d",&choice);
 
->>>>>>> 88fad3fa8ae578fe7ce4af1a16ee382d8ecb956e
-=======
+
+
 	printf("[0]: Quitter\n[1]: Afficher le monde\nVotre choix:");
 	scanf("%d",&choice);
 
->>>>>>> 88fad3fa8ae578fe7ce4af1a16ee382d8ecb956e
+
 	switch(choice){
 		case 0:
 		exit(0);
 		break;
 		case 1: 
 		afficherMonde(monde);
-<<<<<<< HEAD
+
 		break;
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
+
 		break;
-=======
->>>>>>> 88fad3fa8ae578fe7ce4af1a16ee382d8ecb956e
-=======
->>>>>>> 88fad3fa8ae578fe7ce4af1a16ee382d8ecb956e
->>>>>>> 6d454319c10e12fba454cc537b45ff9feaae2cd7
+
+
+
+
+
 		default:
 		printf("NOTHING HERE\n");
 		break;
@@ -76,7 +76,7 @@ void menu(){
 
 }
 
-<<<<<<< HEAD
+
 
 
 
@@ -102,18 +102,18 @@ void afficherListeJoueur(){
 	for(i = 0; i<2; ++i){
 		printf("Joueur %d: ",i+1);
 		afficherJoueur(liste_joueurs[i]);
-=======
+
 void loop(){
 	while(1){
 		menu();
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
 		// test(); //penser à retirer
-=======
->>>>>>> 88fad3fa8ae578fe7ce4af1a16ee382d8ecb956e
-=======
->>>>>>> 88fad3fa8ae578fe7ce4af1a16ee382d8ecb956e
->>>>>>> 6d454319c10e12fba454cc537b45ff9feaae2cd7
+
+
+
+
+
 	}
 }
 
@@ -130,7 +130,7 @@ void selectionnerCase(){
 }
 
 
-<<<<<<< HEAD
+
 void loop(){
 	while(1){
 		for (int i = 0; i < 2; ++i){
@@ -143,29 +143,29 @@ void loop(){
 	}
 }
 
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
+
 void lancer(){
 	monde = creerMonde();
-=======
-=======
->>>>>>> 88fad3fa8ae578fe7ce4af1a16ee382d8ecb956e
->>>>>>> 6d454319c10e12fba454cc537b45ff9feaae2cd7
+
+
+
+
 
 
 void lancer(){
 	monde = creerMonde();
 	initialiserMonde(monde);
-<<<<<<< HEAD
+
 	initialiserListJoueurs();
 	printDelimiteur();
 	afficherListeJoueur();
-=======
-<<<<<<< HEAD
->>>>>>> 88fad3fa8ae578fe7ce4af1a16ee382d8ecb956e
-=======
->>>>>>> 88fad3fa8ae578fe7ce4af1a16ee382d8ecb956e
->>>>>>> 6d454319c10e12fba454cc537b45ff9feaae2cd7
+
+
+
+
+
+
 	loop();
 }
--- monde.h
+++ monde.h
@@ -1,13 +1,13 @@
 typedef struct monde Monde;
 void afficherMonde(Monde *m);
 Monde *creerMonde();
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
 Unite* getUnite(Monde *m, int x, int y);
 int deplaceUnite(Monde *m, int x, int y, int newX, int newY);
-=======
+
 void initialiserMonde(Monde *m);
->>>>>>> 88fad3fa8ae578fe7ce4af1a16ee382d8ecb956e
-=======
+
+
 void initialiserMonde(Monde *m);
->>>>>>> 88fad3fa8ae578fe7ce4af1a16ee382d8ecb956e
+
